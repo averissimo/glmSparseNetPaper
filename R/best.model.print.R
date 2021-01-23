8afea5a724aec77720cc0aa3fd03f5ea35283183
André Veríssimo <afsverissimo@gmail.com>
2021-01-23 15:55:02 +0000

    minor improves on some aesthetics

--- R/best.model.print.R
+++ R/best.model.print.R
@@ -24,15 +24,21 @@ best.model.print <- function(filter.model, best.ones, xdata, ydata, ntimes.resul
   test.ix          <- ntimes.results[[best.ix]]$ixs$test
   coef.list        <- list(ntimes.results[[best.ix]]$coefs[[best.model]])
   names(coef.list) <- filter.model
+  cat('\n\n')
+  cat('#### Test set {-}')
+  cat('\n\n')
   print(
     separate2GroupsCox(coef.list, xdata[test.ix, ], ydata[test.ix, ],
                        plot.title = paste0(prettify.labels(best.model), ' (test set)'),
                        break.x.by = 12)$plot
   )
-
+  cat('\n\n')
+  cat('#### Full set {-}')
+  cat('\n\n')
   print(
     separate2GroupsCox(coef.list, xdata, ydata,
                        plot.title = paste0(prettify.labels(best.model), ' (full data set)'),
                        break.x.by = 12)$plot
   )
+  cat('\n\n')
 }
